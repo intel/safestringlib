961c6084a527b2e163d4129f1a80792b6e7f849a
Dave W <david.m.wheeler@intel.com>
2018-01-26 15:59:04 -0700

    remove unused variable
    overlap_bumper not used

--- safeclib/stpncpy_s.c
+++ safeclib/stpncpy_s.c
@@ -116,7 +116,6 @@ stpncpy_s(char *dest, rsize_t dmax, const char *src, rsize_t smax, errno_t *err)
 {
     rsize_t orig_dmax;
     char *orig_dest;
-    const char *overlap_bumper;
 
     if (dest == NULL) {
         invoke_safe_str_constraint_handler("stpncpy_s: dest is null",
@@ -266,4 +265,4 @@ stpncpy_s(char *dest, rsize_t dmax, const char *src, rsize_t smax, errno_t *err)
     *err = RCNEGATE(ESNOSPC);
     return NULL;
 }
-EXPORT_SYMBOL(stpncpy_s)
+EXPORT_SYMBOL(stpncpy_s);
